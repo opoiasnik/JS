6fda4ae15b9c0b187646aa37af6301babce9f282
Oleh <122808904+opoiasnik@users.noreply.github.com>
2024-05-24 15:10:24 +0200

    Update script.js

--- library/script.js
+++ library/script.js
@@ -4,77 +4,56 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
     let bookButton = document.querySelector('.addBook');
     let form = document.querySelector('.form');
-    let fieldset = document.querySelector('.fieldset');
-    let table = document.querySelector('.table')
-    let submitButton = document.querySelector('.submit')
-    let cancelButton = document.querySelector('.cancel')
-
-
+    let table = document.querySelector('.table tbody');
+    let submitButton = document.querySelector('.submit');
+    let cancelButton = document.querySelector('.cancel');
 
     bookButton.addEventListener('click', function(event) {
         form.style.display = 'block';
-        if(form.style.display === 'block'){
-            let author = document.querySelector('#author')
-                let title = document.querySelector('#title')
-                let pages = document.querySelector('#pages')
-            submitButton.addEventListener('click', function(event){
+        if (form.style.display === 'block') {
+            let author = document.querySelector('#author');
+            let title = document.querySelector('#title');
+            let pages = document.querySelector('#pages');
+            submitButton.addEventListener('click', function(event) {
                 event.preventDefault();
-               
-               
+
                 authorValue = author.value;
                 titleValue = title.value;
                 pagesValue = pages.value;
 
-
                 let addRow = document.createElement('tr');
                 table.appendChild(addRow);
-            
-
-
 
                 let addNumber = document.createElement('td');
-                counter++;
-                addNumber.innerText = `${counter}`;
-                addRow.appendChild(addNumber)
-                
-
-
+                addNumber.innerText = `${++counter}`;
+                addRow.appendChild(addNumber);
 
                 let addColumnAuthor = document.createElement('td');
                 addColumnAuthor.textContent = authorValue;
-                addRow.appendChild(addColumnAuthor)
+                addRow.appendChild(addColumnAuthor);
 
                 let addColumnTitle = document.createElement('td');
-                addColumnTitle.textContent = title.value;
+                addColumnTitle.textContent = titleValue;
                 addRow.appendChild(addColumnTitle);
 
                 let addColumnPages = document.createElement('td');
-                addColumnPages.textContent = pages.value;
+                addColumnPages.textContent = pagesValue;
                 addRow.appendChild(addColumnPages);
 
                 let addCheckboxButton = document.createElement('td');
-                let CheckboxButton = document.createElement('input')
+                let CheckboxButton = document.createElement('input');
                 CheckboxButton.setAttribute('type', 'checkbox');
-                CheckboxButton.setAttribute('class', 'checkbox')
-                addRow.appendChild(addCheckboxButton);
+                CheckboxButton.setAttribute('class', 'checkbox');
                 addCheckboxButton.appendChild(CheckboxButton);
+                addRow.appendChild(addCheckboxButton);
 
-
-                form.reset()
-            })
-            cancelButton.addEventListener('click', function(event){
+                form.reset();
+                form.style.display = 'none';
+            });
+            cancelButton.addEventListener('click', function(event) {
                 event.preventDefault();
                 form.style.display = 'none';
-            })
-
-         }
+            });
+        }
     });
-
-   
-
-
-
-
-
-
 });
