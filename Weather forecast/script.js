a029a6af6bf27648b14255eb3c880f5f0c53a605
Oleh <122808904+opoiasnik@users.noreply.github.com>
2024-05-24 15:00:02 +0200

    Update script.js

--- Weather forecast/script.js
+++ Weather forecast/script.js
@@ -1,34 +1,37 @@
-
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function() {
     let text = document.querySelector('.cityText');
+    let weatherInfo = document.getElementById('weatherInfo');
+    let weatherIcon = document.getElementById('weatherIcon');
+    
     async function getData(city) {
         const apiKey = '20d83d07166d7805dc59557e408874a3';
-        let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+        let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     
-        try{
-            let respond = await fetch(apiUrl);
-            if(respond.ok){
-                return  respond.json();
+        try {
+            let response = await fetch(apiUrl);
+            if (response.ok) {
+                return response.json();
             }
-        }catch(error){
-            alert('Error' + error)
+        } catch (error) {
+            alert('Error: ' + error);
         }
     }
     
-    
-    
-    let city ;
-    let submit = document.querySelector('button')
-    submit.addEventListener('click', async function(){
-        city = document.getElementById('input').value
+    let city;
+    let submit = document.querySelector('button');
+    submit.addEventListener('click', async function() {
+        city = document.getElementById('input').value;
        
-       let objectJson =  await getData(city);
-       let currentTemperature = Math.ceil(objectJson.main.temp - 273.15);
-        
-       
-       text.innerHTML = `City: ${objectJson.name} <br>Country: ${objectJson.sys.country}<br>Temperature: ${currentTemperature} °C`
-
-    })
-})
-
- 
+        let objectJson = await getData(city);
+        if(objectJson) {
+            let currentTemperature = Math.ceil(objectJson.main.temp);
+            let weatherDescription = objectJson.weather[0].description;
+            let weatherIconUrl = `http://openweathermap.org/img/wn/${objectJson.weather[0].icon}@2x.png`;
+            
+            weatherIcon.src = weatherIconUrl;
+            weatherIcon.style.display = 'block';
+            
+            weatherInfo.innerHTML = `City: ${objectJson.name} <br>Country: ${objectJson.sys.country}<br>Temperature: ${currentTemperature} °C<br>Description: ${weatherDescription}`;
+        }
+    });
+});
