0232c4f57c8df69cda9ab01b34168aa6c2fefd47
oleh <oleh@gmail.com>
2024-06-11 13:54:21 +0200

    updated to do list

--- ToDo List/script.js
+++ ToDo List/script.js
@@ -1,5 +1,3 @@
-
-
 document.addEventListener('DOMContentLoaded', function(){
 
     let priority = 0;
@@ -12,225 +10,99 @@ document.addEventListener('DOMContentLoaded', function(){
         }
     }
 
-
     let array = [];
     let addButton = document.querySelector('.addButton');
     let isDisplayBackground = document.querySelector('.formBackground');
-    let headerMain = document.querySelector('.headerMain');
-
-
+    let taskContainer = document.querySelector('.taskContainer');
 
     addButton.addEventListener('click', function(){
-        
-        
-        if ( isDisplayBackground.style.display === 'none') {
-            
+        if (isDisplayBackground.style.display === 'none') {
             isDisplayBackground.style.display = 'block';
-            isDisplayBackground.style.animation = 'appearance 1s ease-in-out'
+            isDisplayBackground.style.animation = 'appearance 1s ease-in-out';
         }
-        
-    })
+    });
 
     let submitButton = document.querySelector('.submitButton');
     submitButton.addEventListener('click', function(event){
         event.preventDefault();
-        
-            let nameValue = document.querySelector('#name').value;
-            let descriptionValue = document.querySelector('#description').value;
-            let dateValue = document.querySelector('#date').value;
-            let obj = new AddPlan(nameValue, descriptionValue, dateValue, priority);
-            array.push(obj);
-             let index = array.length - 1;
-
-            
-            updateDOM();
-             document.querySelector('.form').reset();
-             priority++;
-
-            
-            isDisplayBackground.style.display = 'none'; 
-
-    })
-
-
-
-
+        let nameValue = document.querySelector('#name').value;
+        let descriptionValue = document.querySelector('#description').value;
+        let dateValue = document.querySelector('#date').value;
+        let obj = new AddPlan(nameValue, descriptionValue, dateValue, priority);
+        array.push(obj);
+        priority++;
+        updateDOM();
+        document.querySelector('.form').reset();
+        isDisplayBackground.style.display = 'none';
+    });
 
     function addEventListeners(){
-        let upperImg = document.querySelectorAll('.upperImg')
-        let downImg = document.querySelectorAll('.downImg')
+        let upperImg = document.querySelectorAll('.upperImg');
+        let downImg = document.querySelectorAll('.downImg');
         let deleteImg = document.querySelectorAll('.delete');
         let resetButton = document.querySelector('.rightHeader');
 
         resetButton.addEventListener('click', function(){
             location.reload();
-        })
-    
-        upperImg.forEach( function(element){
-                element.addEventListener('click', function(){
-                    let indexElement = element.getAttribute('data-id');
-    
-                    if(indexElement-1 >=0){
+        });
 
-                        let buffer = array[indexElement-1];
-                        array[indexElement-1] = array[indexElement];
-                        array[indexElement] = buffer;
-                    }
-                    
-                   writeNew();
-                })
-        })
-    
+        upperImg.forEach(function(element){
+            element.addEventListener('click', function(){
+                let indexElement = element.getAttribute('data-id');
+                if (indexElement - 1 >= 0) {
+                    let buffer = array[indexElement - 1];
+                    array[indexElement - 1] = array[indexElement];
+                    array[indexElement] = buffer;
+                }
+                updateDOM();
+            });
+        });
+
         downImg.forEach(function(element){
             element.addEventListener('click', function(){
                 let indexElement = parseInt(element.getAttribute('data-id'));
-        
-                if(indexElement + 1 < array.length){
+                if (indexElement + 1 < array.length) {
                     let buffer = array[indexElement];
                     array[indexElement] = array[indexElement + 1];
                     array[indexElement + 1] = buffer;
-        
-                    writeNew(); 
+                    updateDOM();
                 }
-            })
-        })
-
+            });
+        });
 
         deleteImg.forEach(element => {
             element.addEventListener('click', function(){
                 let indexElement = parseInt(element.getAttribute('data-id'));
-                let itemToDelete = document.querySelector(`.mainList[data-id="${indexElement}"]`);
-                
-                // Удаляем элемент из DOM
-                if (itemToDelete) {
-                    itemToDelete.remove();
-                }
-        
-                // Удаляем объект из массива
-                if (indexElement > -1 && indexElement < array.length) {
-                    array.splice(indexElement, 1);
-                }
-        
-                // Обновляем DOM после удаления
-               
-                 updateDOM();
-            })
+                array.splice(indexElement, 1);
+                updateDOM();
+            });
         });
-        
-        
-        
     }
 
     function updateDOM() {
-        
-        while (headerMain.children.length > 1) {
-            headerMain.removeChild(headerMain.lastChild);
-        }
-    
+        taskContainer.innerHTML = '';
+
         array.forEach((element, index) => {
-            let existingList = document.createElement('ul');
-            existingList.className = 'mainList';
-            existingList.setAttribute('data-id', index);
-    
-            let newListItem1 = document.createElement('li');
-            newListItem1.textContent = element.name;
-            existingList.appendChild(newListItem1);
-    
-            let newListItem2 = document.createElement('li');
-            newListItem2.textContent = element.description;
-            existingList.appendChild(newListItem2);
-    
-            let newListItem3 = document.createElement('li');
-            newListItem3.textContent = element.date;
-            existingList.appendChild(newListItem3);
-    
-            let newListItem5 = document.createElement('img');
-            newListItem5.className = 'upperImg';
-            newListItem5.setAttribute('data-id', index);
-            newListItem5.src = 'up-arrow.png';
-            existingList.appendChild(newListItem5);
-    
-            let newListItem6 = document.createElement('img');
-            newListItem6.className = 'downImg';
-            newListItem6.setAttribute('data-id', index);
-            newListItem6.src = 'down-arrow.png';
-            existingList.appendChild(newListItem6);
-    
-            let newListItem7 = document.createElement('img');
-            newListItem7.className = 'delete';
-            newListItem7.setAttribute('data-id', index);
-            newListItem7.src = 'delete.png';
-            existingList.appendChild(newListItem7);
-    
-            headerMain.appendChild(existingList);
+            let taskRow = document.createElement('div');
+            taskRow.className = 'taskRow';
+            taskRow.setAttribute('data-id', index);
+
+            taskRow.innerHTML = `
+                <div>${element.name}</div>
+                <div>${element.description}</div>
+                <div>${element.date}</div>
+                <div>
+                    <i class="fas fa-arrow-up upperImg" data-id="${index}"></i>
+                    <i class="fas fa-arrow-down downImg" data-id="${index}"></i>
+                    <i class="fas fa-trash delete" data-id="${index}"></i>
+                </div>
+            `;
+
+            taskContainer.appendChild(taskRow);
         });
-    
+
         addEventListeners();
     }
     
-    
-    
-
-
-function writeNew(){
-  let listLi = headerMain.querySelectorAll('li');
-  listLi.forEach((element, index) => {
-    if(index>3){
-
-        element.remove();
-    }
-    
-  })
-let listImg = headerMain.querySelectorAll('img');
-listImg.forEach((element, index) => {
-    element.remove();
-})
-
-  array.forEach((element, index) => {
-    let existingList = document.querySelector(`ul[data-id="${index}"]`)
-    let newListItem1 = document.createElement('li');
-            newListItem1.textContent = element.name;
-            existingList.appendChild(newListItem1);
-    
-            let newListItem2 = document.createElement('li');
-            newListItem2.textContent = element.description;
-            existingList.appendChild(newListItem2);
-    
-            let newListItem3 = document.createElement('li');
-            newListItem3.textContent = element.date;
-            existingList.appendChild(newListItem3);
-    
-            // let newListItem4 = document.createElement('li');
-            // newListItem4.textContent = element.priority;
-            // existingList.appendChild(newListItem4);
-    
-        
-            let newListItem5 = document.createElement('img');
-            newListItem5.className = 'upperImg';
-            newListItem5.setAttribute('data-id', index);
-            newListItem5.src = 'up-arrow.png';
-            existingList.appendChild(newListItem5);
-    
-            let newListItem6 = document.createElement('img');
-            newListItem6.className = 'downImg';
-            newListItem6.setAttribute('data-id', index);
-            newListItem6.src = 'down-arrow.png';
-            existingList.appendChild(newListItem6);
-
-
-            let newListItem7 = document.createElement('img');
-            newListItem7.className = 'delete';
-            newListItem7.setAttribute('data-id', index);
-            newListItem7.src = 'delete.png';
-            existingList.appendChild(newListItem7);
-  })
-  addEventListeners();
-
-
-  
-}
-    
-addEventListeners();
-
-
-})
+    addEventListeners();
+});
